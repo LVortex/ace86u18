ff5b883d321db3d4fd3f894a1e8884de7b838703
sybdata <as15@wt.de>
2018-06-15 15:18:50 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -7,6 +7,6 @@
 /opt/TorrServer/TorrServer-linux-amd64 >/dev/null 2>&1 &
 
 while true; do
-        sleep $2
+        sleep $1
         rm -rf /tmp/state/.ACEStream/.acestream_cache/* /tmp/state/.ACEStream/collected_torrent_files/*
 done
