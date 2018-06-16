abe36c51f4a83991a8ef0be3848267ddf8e6c3d8
sybdata <as15@wt.de>
2018-06-16 18:23:11 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,8 +4,6 @@
 
 /usr/bin/python2.7 /opt/HTTPAceProxy-master/acehttp.py >/dev/null 2>&1 &
 
-/opt/TorrServer/TorrServer-linux-amd64 >/dev/null 2>&1 &
-
 while true; do
         sleep $1
         rm -rf /opt/acestream/androidfs/acestream.engine/.ACEStream/collected_torrent_files/*
